be4b8c026ee1d8a7bbe48543720a780c11976191
Taras <taraskravets95@gmail.com>
2024-02-01 21:24:31 +0200

    feat: add mock-list and product-card

--- front-end/src/components/ItemCard.tsx
+++ front-end/src/components/ItemCard.tsx
@@ -1,5 +1,8 @@
 import React, { FC } from "react";
-import { AvailablePoint } from "./AvalablePoint";
+import { AvailablePoint } from "./AvailablePoint";
+
+import likeEmptyIcon from "../assets/icons/heart-empty.svg";
+import likeIcon from "../assets/icons/heart.svg";
 
 interface ItemCardProps {
   image: string;
@@ -8,6 +11,7 @@ interface ItemCardProps {
   description: string;
   available: boolean;
   whereCanFind: string;
+  liked: boolean;
 }
 
 export const ItemCard: FC<ItemCardProps> = ({
@@ -17,19 +21,27 @@ export const ItemCard: FC<ItemCardProps> = ({
   description,
   available,
   whereCanFind,
+  liked,
 }) => {
   return (
-    <div className="">
-      <img src={image} alt={`photo of ${title}`} />
-      <div>
-        <h4>{title}</h4>
-        <p>{price}</p>
+    <li className="max-w-[300px] w-full bg-bg rounded-xl p-[10px] relative cursor-pointer transition-transform border border-solid border-backgroundCard transform hover:scale-105 hover:border-[#000] duration-700 ">
+      <img
+        src={liked ? likeIcon : likeEmptyIcon}
+        alt="like icon"
+        className="w-[25px] h-[25px] absolute top-[10px] right-[10px] cursor-pointer"
+      />
+      <img src={image} alt={title} className="h-[300px] object-contain" />
+      <div className="flex justify-between items-start">
+        <h4 className="text-l text-primary">{title}</h4>
+        <p className="text-xl text-primary">{price}</p>
       </div>
-      <p>{description}</p>
-      <div>
+      <p className="text-sm text-secondary truncate mt-[6px] mb-[6px]">
+        {description}
+      </p>
+      <div className="flex items-center gap-[5px]">
         <AvailablePoint available={available} />
-        <p>{whereCanFind}</p>
+        <p className="text-primary">{whereCanFind}</p>
       </div>
-    </div>
+    </li>
   );
 };
